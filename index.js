9830a6b4c2304ea6a91b61ad271cb99bcf8be7f9
sauliyo15 <gc.saul.7@gmail.com>
2023-03-20 11:44:46 +0100

    4.Añadir el servidor de socket.io al servidor web

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let app = express();
 let http = require('http').Server(app);
 let path = require('path');
 let port = process.env.PORT || 3000;
+let io = require('socket.io')(http);
 
 //El servidor servira los ficheros situados en la carpeta public
 app.use(express.static(path.join(__dirname, "public")));
@@ -14,4 +15,9 @@ app.get('*', function(req, res, next) {
 //El servidor escucha en el puerto 3000
 http.listen(port, () => {
     console.log('Abre el navegador en http://localhost:' + port);
+});
+
+//Escuchamos cada nueva conexion
+io.on('connection', function(socket) {
+    console.log('Nueva conexion');
 });
