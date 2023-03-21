21bf5348c5246428ec4379e2a747656e1088bc51
sauliyo15 <gc.saul.7@gmail.com>
2023-03-21 19:25:37 +0100

    Ajustes funcionamiento en cualquier servidor

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('*', function(req, res, next) {
 
 //El servidor escucha en el puerto 3000
 http.listen(port, () => {
-    console.log('Abre el navegador en http://192.168.1.133:' + port);
+    console.log('Servidor funcionando en el puerto ' + port);
 });
 
 //Escuchamos cada nueva conexion
